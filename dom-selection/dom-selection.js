0ab46a39e87ce15863c9438a968d7a5082268566
Muhammad Aulia Akbar <2010817210023@mhs.ulm.ac.id>
2023-03-02 14:05:33 +0800

    dom

---
+++ dom-selection/dom-selection.js
@@ -0,0 +1,9 @@
+const judul = document.getElementById('judul') //element
+const psatu = document.getElementById('p')
+
+judul.style.backgroundColor = 'red';
+psatu.style.backgroundColor = 'yellow';
+
+
+
+
