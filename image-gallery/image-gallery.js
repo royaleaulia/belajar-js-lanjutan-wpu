6ba429dcf97e0d9c043f49cad9667232cc0c943b
Muhammad Aulia Akbar <101610494+royaleaulia@users.noreply.github.com>
2023-03-16 13:42:33 +0800

    gabungkan

--- image-gallery/image-gallery.js
+++ image-gallery/image-gallery.js
@@ -2,10 +2,13 @@ container = document.querySelector('.container')
 thumbs = document.querySelectorAll('.thumb')
 jumbo = document.querySelector('.jumbo')
 
-    thumbs.forEach(function(thumb){
-        thumb.addEventListener('click', function(e){
-            container.removeChild(jumbo)
-        })
+thumbs.forEach(function (thumb) {
+    thumb.addEventListener('click', function (e) {
+        jumbo.setAttribute('src', 'img/' + 'x' + '.jpg')
+
+        // container.removeChild(jumbo)
+
     })
-    
-    
+})
+
+
